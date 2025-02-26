306b0f41c6452bcfbce8bd3f88eed733ea663237
joellacasdeschambault <lacas.deschambault.joel@outlook.com>
2025-02-25 22:07:34 -0500

    Add GetMailAttachmentById query endpoint registration in Routes

--- src/MailinatorProxy.API/Features/Mails/Routes.cs
+++ src/MailinatorProxy.API/Features/Mails/Routes.cs
@@ -1,6 +1,7 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using MailinatorProxy.API.Features.Mails.Queries.GetMailAttachmentById;
 using MailinatorProxy.API.Features.Mails.Queries.GetMailAttachments;
 using MailinatorProxy.API.Features.Mails.Queries.GetMailById;
 using MailinatorProxy.API.Features.Mails.Queries.GetMailInbox;
@@ -18,5 +19,6 @@ public static class Routes
         GetMailInboxQueryEndpoint.RegisterRoute(group);
         GetMailByIdQueryEndpoint.RegisterRoute(group);
         GetMailAttachmentsQueryEndpoint.RegisterRoute(group);
+        GetMailAttachmentByIdQueryEndpoint.RegisterRoute(group);
     }
 }
